a03163dc546523aa6ea6fbcfda8d878dfb072240
Adam Passey <adamjpassey@gmail.com>
2014-06-13 23:03:24 -0700

    Event interface and implementation

---
+++ Assets/Scripts/Event/EventImpl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AdamPassey.Event
+{
+	public class EventImpl : Event
+	{
+		private Object sender;
+
+		public Object Sender {
+			get { return sender; }
+			set { sender = value; }
+		}
+
+	}
+}
