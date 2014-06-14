6dfc2a695c742b63268b8677c335494f2cbc9095
Adam Passey <adamjpassey@gmail.com>
2014-06-14 11:43:27 -0700

    EventUsage includes example of sending / receiving sender

--- Assets/Scripts/Event/EventUsage.cs
+++ Assets/Scripts/Event/EventUsage.cs
@@ -10,17 +10,28 @@ public class EventUsage : MonoBehaviour
 		//	Use Events.When() to register event handlers
 		//	to specific events.
 		Events.When("some-event-happens", EventHandler);
+		Events.When("another-event-happens-send-myself", OtherEventHandler);
 
 		//	pausing for effect
 		Invoke("FireListeners", 5f);
 
 		//	check the console in this example
-		Debug.Log("Event registered. Invoking event in 5 seconds.");
+		Debug.Log("Events registered. Invoking event in 5 seconds.");
 	}
 
 	//	The event handler for "some-event-happens"
-	public void EventHandler() {
-		Debug.Log("Received event.");
+	public void EventHandler(EventContext e) {
+		Debug.Log("Received event: " + e.eventName);
+	}
+
+	//	The event handler for "another-event-happens-send-myself"
+	public void OtherEventHandler(EventContext e) {
+		Debug.Log("Received event: " + e.eventName);
+
+		//	Watch out, sender 'can' be null
+		if (e.sender != null) {
+			Debug.Log("From sender: " + e.sender);
+		}
 	}
 
 	//	Fires an event as an example
@@ -28,5 +39,9 @@ public class EventUsage : MonoBehaviour
 
 		//	Fire an event- notifying all listeners
 		Events.Fire("some-event-happens");
+
+		//	Fire an event- notifying all listeners
+		//	and passing this as sender
+		Events.Fire("another-event-happens-send-myself", this);
 	}
 }
