71f4ed67d1e40b94801b42ca5dc2707e7d6b5b94
Adam Passey <adamjpassey@gmail.com>
2014-06-13 23:04:36 -0700

    Events usage example

---
+++ Assets/Scripts/Event/EventUsage.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using AdamPassey.Event;
+
+public class EventUsage : MonoBehaviour
+{
+	//	Start
+	void Start() {
+		//	Use Events.When() to register event handlers
+		//	to specific events.
+		Events.When("some-event-happens", EventHandler);
+
+		//	pausing for effect
+		Invoke("FireListeners", 5f);
+
+		//	check the console in this example
+		Debug.Log("Event registered. Invoking event in 5 seconds.");
+	}
+
+	//	The event handler for "some-event-happens"
+	public void EventHandler() {
+		Debug.Log("Received event.");
+	}
+
+	//	Fires an event as an example
+	private void FireListeners() {
+
+		//	Fire an event- notifying all listeners
+		Events.Fire("some-event-happens");
+	}
+}
