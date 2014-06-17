26c5cae1f93475c70ebf8e929d5515f97aa0b6f7
Adam Passey <adamjpassey@gmail.com>
2014-06-16 22:26:04 -0700

    Adding a Camera controller to show examples of Parallax

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+	public float speed = 25f;
+
+	// Use this for initialization
+	void Start() {
+	
+	}
+	
+	// Update is called once per frame
+	void Update() {
+
+		Vector2 newPosition = new Vector2();
+
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			newPosition.x -= 1.0f;
+		}
+
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			newPosition.x += 1.0f;
+		}
+
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			newPosition.y += 1.0f;
+		}
+
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			newPosition.y -= 1.0f;
+		}
+
+		transform.Translate(newPosition * speed * Time.deltaTime);
+	}
+}
