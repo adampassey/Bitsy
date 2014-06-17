dc868f5594ebe6dc464b4ebb732a4b3cb8ceba77
Adam Passey <adamjpassey@gmail.com>
2014-06-16 22:27:04 -0700

    Circular parallax script creation

---
+++ Assets/Scripts/Parallax/CircularParallax.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AdamPassey.Parallax
+{
+	public class CircularParallax : MonoBehaviour
+	{
+		public GameObject spriteRendererObject;
+		public float rotationSpeed;
+		public Vector3 rotationOffset;
+
+		private Camera mainCamera;
+		private Vector3 mainCameraOffset;
+		private SpriteRenderer spriteRenderer;
+
+		// Use this for initialization
+		void Start() {
+
+			//	grab the main camera
+			mainCamera = Camera.main;
+			if (mainCamera == null) {
+				Debug.LogError("Circular Parallax requires a main camera to work.");
+			}
+
+			//	grab the camera offset
+			mainCameraOffset = transform.position - mainCamera.transform.position;
+
+			//	initialize the sprite renderer
+			spriteRenderer = spriteRendererObject.GetComponent<SpriteRenderer>();
+			if (spriteRenderer == null) {
+				Debug.LogError("Cicrular Parallax requires a sprite renderer to work.");
+			}
+		}
+	
+		// Update is called once per frame
+		void Update() {
+			transform.position = mainCamera.transform.position + mainCameraOffset + rotationOffset;
+			spriteRenderer.transform.Rotate(Vector3.back * rotationSpeed * Time.deltaTime, Space.World);
+		}
+	}
+}
