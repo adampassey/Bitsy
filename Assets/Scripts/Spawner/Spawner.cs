0de39c264397d5c034f6a26ac56177ddd6026922
Adam Passey <adamjpassey@gmail.com>
2014-06-18 22:04:28 -0700

    Adding Player and Spawner script

---
+++ Assets/Scripts/Spawner/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AdamPassey.Spawner
+{
+	public class Spawner : MonoBehaviour
+	{
+
+		public GameObject prefabToSpawn;
+
+		// Use this for initialization
+		void Start() {
+			GameObject.Instantiate(prefabToSpawn, transform.position, Quaternion.identity);
+		}
+
+		public void OnDrawGizmos() {
+			Gizmos.DrawWireSphere(transform.position, 1f);
+		}
+
+		public void Update() {
+			Destroy(gameObject);
+		}
+	}
+
+}
