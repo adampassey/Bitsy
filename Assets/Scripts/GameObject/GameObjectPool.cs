ea6b60a8ff838976ec7d290cd6ba7004d2a5e1dd
Adam Passey <adamjpassey@gmail.com>
2014-06-28 10:45:33 -0700

    First pass at Game Object Pool

---
+++ Assets/Scripts/GameObject/GameObjectPool.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AdamPassey.Pool
+{
+	public class GameObjectPool<T> where T : MonoBehaviour
+	{
+		private T[] gameObjects;
+
+		/**
+		 *	Create an instance of the GameObjectPool with
+		 *	the prefab and the count of allowable prefabs
+		 *	@param prefab The prefab to spawn
+		 *	@param count The total number of spawned prefabs
+		 **/
+		public GameObjectPool(GameObject prefab, int count)
+		{
+			gameObjects = new T[count];
+
+			for (int i = 0; i < count; i++) {
+				GameObject go = (GameObject)GameObject.Instantiate(prefab, Vector3.zero, Quaternion.identity);
+				go.SetActive(false);
+
+				T co = go.GetComponent<T>();
+				gameObjects[i] = co;
+			}
+		}
+
+		/**
+		 * 	Get an item from the pool
+		 * 	May return null if no objects are available
+		 * 	@returns T The type of object placed in the pool
+		 **/
+		public T Get() {
+
+			//	TODO: this could be more efficient. consider having
+			//	two lists of types, one for active and the other for
+			//	inactive. push / pop between them.
+			foreach (T go in gameObjects) {
+				if (go.gameObject.activeSelf == false) {
+					go.gameObject.SetActive(true);
+					return go;
+				}
+			}
+			return null;
+		}
+	}
+}
