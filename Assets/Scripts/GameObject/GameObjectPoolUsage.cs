7e8f0d7a3cbdb049010bb311fd36e3188de2eae1
Adam Passey <adamjpassey@gmail.com>
2014-06-28 10:46:08 -0700

    Game Object Pool Usage script

---
+++ Assets/Scripts/GameObject/GameObjectPoolUsage.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace AdamPassey.Pool
+{
+
+	public class GameObjectPoolUsage : MonoBehaviour
+	{
+
+		public GameObject poolPrefab;
+		public int poolCount = 10;
+
+		private GameObjectPool<RollingBall> gameObjectPool;
+
+		// Use this for initialization
+		void Start() {
+			gameObjectPool = new GameObjectPool<RollingBall>(poolPrefab, poolCount);
+		}
+	
+		// Update is called once per frame
+		void Update() {
+
+			//	Example of creating an object from the pool
+			if (Input.GetKeyDown(KeyCode.G)) {
+				RollingBall go = gameObjectPool.Get();
+				if (go != null) {
+					Debug.Log("Spawned an object from the pool.");
+
+					//	Do not destroy your object with GameObject.Destroy,
+					//	instead deactivate it with go.SetActive(false)
+					//	this will keep it in the pool
+				}
+			}
+		}
+	}
+}
