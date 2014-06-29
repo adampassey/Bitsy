80c0eff272d77af4271fe30612c1dfa73ad4dc1f
Adam Passey <adamjpassey@gmail.com>
2014-06-29 13:01:08 -0700

    Simple Inventory component for Adding / Removing objects

---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+using AdamPassey.GameObjectHelper;
+
+namespace AdamPassey.Inventory
+{
+	[AddComponentMenu("Gameplay/Inventory")]
+	public class Inventory : MonoBehaviour
+	{
+
+		public List<GameObject> inventory;
+		private GameObject parent;
+		private static string parentName = "Inventory";
+
+		public void Start() {
+			inventory = new List<GameObject>();
+			parent = GameObjectFactory.NewGameObject(parentName, gameObject.transform);
+		}
+
+		/**
+		 * 	Add an object to the inventory
+		 * 	@param obj The GameObject
+		 **/
+		public void AddObject(GameObject obj) {
+			inventory.Add(obj);
+			obj.SetActive(false);
+			obj.transform.parent = parent.transform;
+		}
+
+		/**
+		 * 	Get an object from the inventory at
+		 * 	the given index
+		 * 	@param index The index
+		 **/
+		public GameObject GetObject(int index) {
+			GameObject obj = inventory[index];
+			inventory.RemoveAt(index);
+			obj.SetActive(true);
+			obj.transform.parent = null;
+			return obj;
+		}
+	}
+}
