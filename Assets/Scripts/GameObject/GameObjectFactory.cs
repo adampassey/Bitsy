81a93354b5b640a697e9a73e99fa6b5df05a5a3a
Adam Passey <adamjpassey@gmail.com>
2014-06-29 12:56:59 -0700

    Game Object Factory for ease of creating new Game Objects

---
+++ Assets/Scripts/GameObject/GameObjectFactory.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AdamPassey.GameObjectHelper
+{
+	public class GameObjectFactory
+	{
+		/**
+		 * 	Create a GameObject with name
+		 * 	@param name The name of the GameObject
+		 **/
+		public static GameObject NewGameObject(string name) {
+			GameObject obj = new GameObject();
+			obj.name = name;
+			return obj;
+		}
+
+		/**
+		 * 	Create a GameObject with a name and a
+		 * 	parent transform
+		 * 	@param name The name of the GameObject
+		 * 	@param parent The parent transform
+		 **/
+		public static GameObject NewGameObject(string name, Transform parent) {
+			GameObject obj = NewGameObject(name);
+			obj.transform.parent = parent;
+			return obj;
+		}
+	}
+}
