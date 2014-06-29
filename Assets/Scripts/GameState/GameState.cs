816a6df421cadc902c316d4121ccd4af8124345d
Adam Passey <adamjpassey@gmail.com>
2014-06-29 13:14:14 -0700

    Static Game State class

---
+++ Assets/Scripts/GameState/GameState.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AdamPassey.GameState
+{
+	public class GameState
+	{
+		public enum States
+		{
+			Paused,
+			Running
+		}
+		;
+
+		//	Running by default
+		private static States state = States.Running;
+
+		/**
+		 * 	Get the current Game State
+		 **/
+		public static States State() {
+			return state;
+		}
+
+		/**
+		 * 	Set the current Game State
+		 * 	@param gameState The Game State
+		 **/
+		public static void SetState(States gameState) {
+			state = gameState;
+		}
+
+		/**
+		 * 	Convenience method for checking if the
+		 * 	Game State is running
+		 **/
+		public static bool IsRunning() {
+			return state == States.Running;
+		}
+
+		/**
+		 * 	Convenience method for checking if the
+		 * 	Game State is paused
+		 **/
+		public static bool IsPaused() {
+			return state == States.Paused;
+		}
+	}
+
+}
