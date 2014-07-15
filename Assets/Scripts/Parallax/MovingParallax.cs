46ccbeb404dd03d5dda00ef3acb4568951e06258
Adam Passey <adamjpassey@gmail.com>
2014-07-14 22:53:18 -0700

    Moving Parallax component
    This component duplicates a lot of the code found in the main Parallax
    script. This should be refactored into a base class or shared component.

--- Assets/Scripts/Parallax/MovingParallax.cs
+++ Assets/Scripts/Parallax/MovingParallax.cs
@@ -3,42 +3,43 @@ using System.Collections;
 
 namespace AdamPassey.Parallax
 {
-	[AddComponentMenu("Environment/Parallax")]
-	public class Parallax : MonoBehaviour
+	[AddComponentMenu("Environment/Moving Parallax")]
+	public class MovingParallax : MonoBehaviour
 	{
+
 		public GameObject textureObject;
 		public float speed = 0.1f;
-
+	
 		private Camera mainCamera;
 		private Vector3 previousPosition;
 		private Vector3 offset;
 
 		// Use this for initialization
 		void Start() {
-	
 			mainCamera = Camera.main;
 			if (mainCamera == null) {
-				Debug.LogError("Parallax requires a main camera to work.");
+				Debug.LogError("Moving Parallax requires a main camera to work.");
 			}
-
+			
 			offset = mainCamera.transform.position - transform.position;
 		}
 	
 		// Update is called once per frame
 		void Update() {
+
+			//	TODO: this is duplicate code from the Parallax
+			//	script. Create a shared component to handle the
+			//	common functionality between Parallax components
 			Vector3 currentPosition = mainCamera.transform.position;
 
-			//	create the texture offset based on desired speed
 			Vector2 textureOffset = textureObject.renderer.material.mainTextureOffset;
-			textureOffset.x += (currentPosition.x - previousPosition.x) * speed;
+			textureOffset.x += speed * Time.deltaTime;
 			textureObject.renderer.material.mainTextureOffset = textureOffset;
 
-			//	set the y position based on speed
 			Vector3 newPosition = currentPosition;
 			newPosition.y -= previousPosition.y * speed;
 
-			//	update the parallax position
-			transform.position = newPosition - offset;
+			textureObject.transform.position = currentPosition - offset;
 			previousPosition = mainCamera.transform.position;
 		}
 	}
